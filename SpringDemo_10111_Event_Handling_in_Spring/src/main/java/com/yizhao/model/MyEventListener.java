5ee16ee4471dee56e07dfa76dcd8a8fcadf14727
Yi Zhao <yizhao.cs@gmail.com>
2015-02-06 13:46:57 -0800

    1

---
+++ SpringDemo_10111_Event_Handling_in_Spring/src/main/java/com/yizhao/model/MyEventListener.java
@@ -0,0 +1,16 @@
+package com.yizhao.model;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyEventListener implements ApplicationListener{
+
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		System.out.println(event.toString());
+		
+	}
+
+}
